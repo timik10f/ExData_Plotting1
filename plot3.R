29a94bc4e9522b26d08663bf68513b5428632532
Timur Akhmedzhanov <timik10f@tamu.edu>
2015-12-13 17:15:32 -0600

    Second Commit

--- plot3.R
+++ plot3.R
@@ -9,7 +9,8 @@ powerConsData$TimeDate <- as.POSIXct(paste(powerConsData$Date, powerConsData$Tim
 powerConsData$Date <- as.Date(powerConsData$Date,format="%d/%m/%Y") # converting dates to different format
 powerConsData <- subset(powerConsData, (powerConsData$Date == "2007-02-01"|powerConsData$Date == "2007-02-02"))# subsetting data
 
-# plotting on screen
+# plotting to png
+png('plot3.png')
 par(mfrow=c(1,1))
 plot(powerConsData$TimeDate, as.numeric(as.character(powerConsData$Sub_metering_1)),type = "n",xlab ="",ylab = "Energy sub metering")
 lines(powerConsData$TimeDate, as.numeric(as.character(powerConsData$Sub_metering_1))) 
@@ -18,9 +19,7 @@ lines(powerConsData$TimeDate, as.numeric(as.character(powerConsData$Sub_metering
 lines(powerConsData$TimeDate, as.numeric(as.character(powerConsData$Sub_metering_3)), col = "blue") 
 par(new=F)
 #adding legend
+par(cex = 1.0)
 legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("black","red","blue"))
 
-
-#copying to png device
-dev.copy(png,'plot3.png')
 dev.off()
